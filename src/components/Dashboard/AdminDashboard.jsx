b46b14183ed2dd3df89286be8c1606165f52fab0
BDbrijgit <brijtest09@gmail.com>
2024-10-24 17:51:54 +0530

    Initial Commit

---
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../Other/Header";
+
+const AdminDashboard = () => {
+  return (
+    <div className="h-screen w-full p-10">
+      <Header />
+
+      <div>
+        <form>
+          <h3>Task Title</h3>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AdminDashboard;
---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+
+const TaskList = () => {
+  return (
+    <div
+      id="taskList"
+      className="h-[55%] overflow-x-auto flex items-center justify-start gap-5 py-5 flex-nowrap w-full mt-10"
+    >
+      <div className="h-full w-[300px] shrink-0 p-5 bg-[#5e35b1] rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm mt-2">20-October-2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a NodeJs Server</h2>
+        <p className="text-sm">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi,
+          voluptates soluta provident nihil beatae dolorem maiores harum dolore
+          aliquam doloremque.
+        </p>
+      </div>
+      <div className="h-full w-[300px] shrink-0 p-5 bg-[#3949ab] rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm mt-2">20-October-2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a NodeJs Server</h2>
+        <p className="text-sm">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi,
+          voluptates soluta provident nihil beatae dolorem maiores harum dolore
+          aliquam doloremque.
+        </p>
+      </div>
+      <div className="h-full w-[300px] shrink-0 p-5 bg-[#6d4c41] rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm mt-2">20-October-2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a NodeJs Server</h2>
+        <p className="text-sm">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi,
+          voluptates soluta provident nihil beatae dolorem maiores harum dolore
+          aliquam doloremque.
+        </p>
+      </div>
+
+      <div className="h-full w-[300px] shrink-0 p-5 bg-[#01579b] rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm mt-2">20-October-2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a NodeJs Server</h2>
+        <p className="text-sm">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi,
+          voluptates soluta provident nihil beatae dolorem maiores harum dolore
+          aliquam doloremque.
+        </p>
+      </div>
+      <div className="h-full w-[300px] shrink-0 p-5 bg-[#37474f] rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm mt-2">20-October-2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a NodeJs Server</h2>
+        <p className="text-sm">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi,
+          voluptates soluta provident nihil beatae dolorem maiores harum dolore
+          aliquam doloremque.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TaskList;
